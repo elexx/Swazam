92de73455b3d542811aaa39c56c3bddd3cf4cca2
Alexander Falb <fal3xx@gmail.com>
2012-11-23 14:06:42 +0100

    initial project layout

---
+++ Server/src/main/java/swa/swazam/server/App.java
@@ -0,0 +1,13 @@
+package swa.swazam.server;
+
+import swa.swazam.util.MyUtil;
+
+/**
+ * Hello world!
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World Server");
+		System.out.println(new MyUtil().getIt());
+	}
+}
---
+++ Util/src/main/java/swa/swazam/util/MyUtil.java
@@ -0,0 +1,7 @@
+package swa.swazam.util;
+
+public class MyUtil {
+	public String getIt() {
+		return "this is from Util";
+	}
+}
