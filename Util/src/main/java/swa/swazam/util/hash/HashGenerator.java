3fe472afa1713e357745cf1cb4996b84dc9bbfae
Alexander Falb <fal3xx@gmail.com>
2012-11-28 18:20:07 +0100

    implemented hashing and unit tests

--- Util/src/main/java/swa/swazam/util/hash/HashGenerator.java
+++ Util/src/main/java/swa/swazam/util/hash/HashGenerator.java
@@ -1,14 +1,30 @@
 package swa.swazam.util.hash;
 
 public class HashGenerator {
-	
+
 	/**
-	 * Generates the hash value of the given plain text 
+	 * are use for Blowfish's salt generation
+	 */
+	static final int ROUNDS = 10;
+
+	/**
+	 * Generates the hash value of the given plain text
+	 * 
 	 * @param password the text that should be hashed
 	 * @return the hashed text
 	 */
-	public static String hash(String password){
-		return password;
+	public static String hash(String password) {
+		return BCrypt.hashpw(password, BCrypt.gensalt(ROUNDS));
 	}
 
+	/**
+	 * Checks whether the paintext matches the given hashed password or not
+	 * 
+	 * @param plaintext
+	 * @param hashed
+	 * @return
+	 */
+	public static boolean checkPassword(String plaintext, String hashed) {
+		return BCrypt.checkpw(plaintext, hashed);
+	}
 }
---
+++ Util/src/test/java/swa/swazam/util/hash/HashGeneratorTest.java
@@ -0,0 +1,52 @@
+package swa.swazam.util.hash;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HashGeneratorTest {
+	private static final int PLAINTEXT_INDEX = 0;
+	// unused private static final int SALT_INDEX = 1;
+	private static final int HASH_INDEX = 2;
+
+	private Map<Integer, String[][]> testVectors;
+
+	@Before
+	public void resetTestVectors() {
+		testVectors = new HashMap<>();
+		testVectors.put(6, new String[][] { { "", "$2a$06$DCq7YPn5Rq63x1Lad4cll.", "$2a$06$DCq7YPn5Rq63x1Lad4cll.TV4S6ytwfsfvkgY8jIucDrjc8deX1s." }, { "a", "$2a$06$m0CrhHm10qJ3lXRY.5zDGO", "$2a$06$m0CrhHm10qJ3lXRY.5zDGO3rS2KdeeWLuGmsfGlMfOxih58VYVfxe" }, { "abc", "$2a$06$If6bvum7DFjUnE9p2uDeDu", "$2a$06$If6bvum7DFjUnE9p2uDeDu0YHzrHM6tf.iqN8.yx.jNN1ILEf7h0i" }, { "abcdefghijklmnopqrstuvwxyz", "$2a$06$.rCVZVOThsIa97pEDOxvGu", "$2a$06$.rCVZVOThsIa97pEDOxvGuRRgzG64bvtJ0938xuqzv18d3ZpQhstC" }, { "~!@#$%^&*()      ~!@#$%^&*()PNBFRD", "$2a$06$fPIsBO8qRqkjj273rfaOI.", "$2a$06$fPIsBO8qRqkjj273rfaOI.HtSV9jLDpTbZn782DC6/t7qT67P6FfO" } });
+		testVectors.put(8, new String[][] { { "", "$2a$08$HqWuK6/Ng6sg9gQzbLrgb.", "$2a$08$HqWuK6/Ng6sg9gQzbLrgb.Tl.ZHfXLhvt/SgVyWhQqgqcZ7ZuUtye" }, { "a", "$2a$08$cfcvVd2aQ8CMvoMpP2EBfe", "$2a$08$cfcvVd2aQ8CMvoMpP2EBfeodLEkkFJ9umNEfPD18.hUF62qqlC/V." }, { "abc", "$2a$08$Ro0CUfOqk6cXEKf3dyaM7O", "$2a$08$Ro0CUfOqk6cXEKf3dyaM7OhSCvnwM9s4wIX9JeLapehKK5YdLxKcm" }, { "abcdefghijklmnopqrstuvwxyz", "$2a$08$aTsUwsyowQuzRrDqFflhge", "$2a$08$aTsUwsyowQuzRrDqFflhgekJ8d9/7Z3GV3UcgvzQW3J5zMyrTvlz." }, { "~!@#$%^&*()      ~!@#$%^&*()PNBFRD", "$2a$08$Eq2r4G/76Wv39MzSX262hu", "$2a$08$Eq2r4G/76Wv39MzSX262huzPz612MZiYHVUJe/OcOql2jo4.9UxTW" }, });
+		testVectors.put(10, new String[][] { { "", "$2a$10$k1wbIrmNyFAPwPVPSVa/ze", "$2a$10$k1wbIrmNyFAPwPVPSVa/zecw2BCEnBwVS2GbrmgzxFUOqW9dk4TCW" }, { "a", "$2a$10$k87L/MF28Q673VKh8/cPi.", "$2a$10$k87L/MF28Q673VKh8/cPi.SUl7MU/rWuSiIDDFayrKk/1tBsSQu4u" }, { "abc", "$2a$10$WvvTPHKwdBJ3uk0Z37EMR.", "$2a$10$WvvTPHKwdBJ3uk0Z37EMR.hLA2W6N9AEBhEgrAOljy2Ae5MtaSIUi" }, { "abcdefghijklmnopqrstuvwxyz", "$2a$10$fVH8e28OQRj9tqiDXs1e1u", "$2a$10$fVH8e28OQRj9tqiDXs1e1uxpsjN0c7II7YPKXua2NAKYvM6iQk7dq" }, { "~!@#$%^&*()      ~!@#$%^&*()PNBFRD", "$2a$10$LgfYWkbzEvQ4JakH7rOvHe", "$2a$10$LgfYWkbzEvQ4JakH7rOvHe0y8pHKF9OaFgwUZ2q7W2FFZmZzJYlfS" }, });
+		testVectors.put(12, new String[][] { { "", "$2a$12$k42ZFHFWqBp3vWli.nIn8u", "$2a$12$k42ZFHFWqBp3vWli.nIn8uYyIkbvYRvodzbfbK18SSsY.CsIQPlxO" }, { "a", "$2a$12$8NJH3LsPrANStV6XtBakCe", "$2a$12$8NJH3LsPrANStV6XtBakCez0cKHXVxmvxIlcz785vxAIZrihHZpeS" }, { "abc", "$2a$12$EXRkfkdmXn2gzds2SSitu.", "$2a$12$EXRkfkdmXn2gzds2SSitu.MW9.gAVqa9eLS1//RYtYCmB1eLHg.9q" }, { "abcdefghijklmnopqrstuvwxyz", "$2a$12$D4G5f18o7aMMfwasBL7Gpu", "$2a$12$D4G5f18o7aMMfwasBL7GpuQWuP3pkrZrOAnqP.bmezbMng.QwJ/pG" }, { "~!@#$%^&*()      ~!@#$%^&*()PNBFRD", "$2a$12$WApznUOJfkEGSmYRfnkrPO", "$2a$12$WApznUOJfkEGSmYRfnkrPOr466oFDCaj4b6HY3EXGvfxm43seyhgC" } });
+	}
+
+	/**
+	 * for sake of speed we run only a subset of testcases, defined by {@link HashGenerator#ROUNDS}
+	 */
+	@Test
+	public void testCheckPassword() {
+		boolean onlyMatchings = true;
+		for (String[] testVector : testVectors.get(HashGenerator.ROUNDS)) {
+			onlyMatchings &= HashGenerator.checkPassword(testVector[PLAINTEXT_INDEX], testVector[HASH_INDEX]);
+		}
+		assertTrue(onlyMatchings);
+	}
+
+	/**
+	 * for sake of speed we run only a subset of testcases, defined by {@link HashGenerator#ROUNDS}
+	 */
+	@Test
+	public void testCheckPassword2() {
+		boolean onlyMissMatchings = false;
+		for (String[] testVector : testVectors.get(HashGenerator.ROUNDS)) {
+			testVector[PLAINTEXT_INDEX] += "!";
+			onlyMissMatchings |= HashGenerator.checkPassword(testVector[PLAINTEXT_INDEX], testVector[HASH_INDEX]);
+		}
+		assertFalse(onlyMissMatchings);
+	}
+}
