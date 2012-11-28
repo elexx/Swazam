e423ee909e6d8eac62b71509bdc617b58d04b2a9
christina zrelski <christina.zrelski@gmail.com>
2012-11-28 08:21:58 +0100

    added hash package and class to util

---
+++ Util/src/main/java/swa/swazam/util/hash/HashGenerator.java
@@ -0,0 +1,14 @@
+package swa.swazam.util.hash;
+
+public class HashGenerator {
+	
+	/**
+	 * Generates the hash value of the given plain text 
+	 * @param password the text that should be hashed
+	 * @return the hashed text
+	 */
+	public static String hash(String password){
+		return password;
+	}
+
+}
