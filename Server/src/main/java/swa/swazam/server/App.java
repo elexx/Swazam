b720140df5bf56b443801970afc99fba30b3c899
Alexander Falb <fal3xx@gmail.com>
2012-11-29 10:35:07 +0100

    jetty embedded up and running

--- Server/src/main/java/swa/swazam/server/App.java
+++ Server/src/main/java/swa/swazam/server/App.java
@@ -1,50 +1,34 @@
 package swa.swazam.server;
 
-import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.util.resource.ResourceCollection;
 import org.eclipse.jetty.webapp.WebAppContext;
-
-import swa.swazam.util.MyUtil;
 
 /**
  * Hello world!
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World Server");
-		System.out.println(new MyUtil().getIt());
-	
-		startServer();
-	}
-	
-	private static void startServer(){
-		ContextHandlerCollection contexts = new ContextHandlerCollection();
-		contexts.setHandlers(new Handler[] {createWebAppContext()});
-		
-		final Server server = new Server(8080);
-		server.setHandler(contexts);
-		Runnable thread = new Runnable(){
+		String webappDirLocation = "src/main/webapp/";
 
-			@Override
-			public void run() {
-				try {
-					server.start();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		};
-		
-		thread.run();
-	}
-	
-	private static WebAppContext createWebAppContext(){
-		WebAppContext webAppContext = new WebAppContext();
-		webAppContext.setDescriptor("/WEB-INF/web.xml");
-		webAppContext.setResourceBase(".");
-		webAppContext.setContextPath("/");
-		
-		return webAppContext;
+		Server server = new Server(8080);
+		WebAppContext root = new WebAppContext();
+
+		root.setContextPath("/");
+		root.setDescriptor(webappDirLocation + "WEB-INF/web.xml");
+		root.setResourceBase(webappDirLocation);
+		root.setBaseResource(new ResourceCollection(new String[] { webappDirLocation, "target/" }));
+		root.setResourceAlias("/WEB-INF/classes/", "/classes/");
+
+		root.setParentLoaderPriority(true);
+
+		server.setHandler(root);
+
+		try {
+			server.start();
+			server.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- Server/src/main/java/swa/swazam/server/web/TestBean.java
+++ Server/src/main/java/swa/swazam/server/web/TestBean.java
@@ -1,12 +1,14 @@
 package swa.swazam.server.web;
 
 import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 @ManagedBean(name = "TestBean")
 @SessionScoped
 public class TestBean implements Serializable {
+	private static final long serialVersionUID = 7210211154402593080L;
 
 	private String test = "Hello World";
 
