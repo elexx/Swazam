b88f10dcec10580248668f09b8e0ec88274dc7b8
Markus Zoffi <markus.zoffi@gmail.com>
2012-11-30 18:59:23 +0100

    Request an DB angebunden und getestet

--- Server/src/main/java/swa/swazam/server/JPATester.java
+++ Server/src/main/java/swa/swazam/server/JPATester.java
@@ -1,9 +1,13 @@
 package swa.swazam.server;
 
+import java.util.Date;
+import java.util.UUID;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import swa.swazam.server.entity.Request;
 import swa.swazam.server.entity.User;
 
 public class JPATester {
@@ -12,9 +16,13 @@ public class JPATester {
 		EntityManager em = emf.createEntityManager();
 		
 		User user = new User("christina", "12345", "Christina", "Zrelski", "christina.zrelski@gmail.com", 100);
-		
+				
 		em.getTransaction().begin();
-		em.persist(user);
+		//em.persist(user);
+		User u = em.find(User.class, "christina");
+		
+		Request request = new Request("Bards song", "Blind guardian", new Date(System.currentTimeMillis()), u, null, false, UUID.randomUUID());
+		em.persist(request);
 		em.getTransaction().commit();
 	}
 
