ab0fb32187353b73969b3411991b6dade45061a8
Markus Zoffi <markus.zoffi@gmail.com>
2012-12-29 12:44:18 +0100

    changed javadoc

--- Server/src/main/java/swa/swazam/server/dao/HistoryDao.java
+++ Server/src/main/java/swa/swazam/server/dao/HistoryDao.java
@@ -10,7 +10,7 @@ public interface HistoryDao {
 	 * Retrieves all recognition requests that the given user has
 	 * requested.
 	 * @param username the username of the user
-	 * @return a list containing all found recognition requests or null
+	 * @return a list containing all found recognition requests or an empty list
 	 * if no recognition requests were found.
 	 */
 	public List<Request> getAllRequestedRequestsFromUser(String username);
@@ -19,7 +19,7 @@ public interface HistoryDao {
 	 * Retrieves all recognition requests that the given user has
 	 * solved.
 	 * @param username the username of the user
-	 * @return a list containing all found recognition requests or null
+	 * @return a list containing all found recognition requests or an empty list
 	 * if no recognition requests were found.
 	 */
 	public List<Request> getAllSolvedRequestsFromUser(String username);
