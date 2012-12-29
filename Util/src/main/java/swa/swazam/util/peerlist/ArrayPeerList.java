c55caef875276573e3750b27439dd98141505dc7
Michael Borkowski <michael.borkowski@tuwien.ac.at>
2012-12-29 11:40:32 +0100

    generic peer list

---
+++ Util/src/main/java/swa/swazam/util/peerlist/ArrayPeerList.java
@@ -0,0 +1,26 @@
+package swa.swazam.util.peerlist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayPeerList<T> extends ArrayList<T> implements PeerList<T> {
+
+	private static final long serialVersionUID = 974679576323988400L;
+
+	@Override
+	public void push(T peer) {
+		super.remove(peer);
+		super.add(0, peer);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public T[] getTopArray(int n) {
+		return (T[]) getTop(n).toArray();
+	}
+
+	@Override
+	public List<T> getTop(int n) {
+		return super.subList(0, Math.min(n, super.size()));
+	}
+}
