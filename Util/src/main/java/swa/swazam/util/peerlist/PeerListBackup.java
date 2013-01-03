b3fc0310b8692f52288a2f3e3698ceadf4d62d8a
christina zrelski <christina.zrelski@gmail.com>
2013-01-03 17:00:52 +0100

    serialisation/deserialisation peerlist to filesystem

---
+++ Util/src/main/java/swa/swazam/util/peerlist/PeerListBackup.java
@@ -0,0 +1,71 @@
+package swa.swazam.util.peerlist;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+
+import swa.swazam.util.exceptions.SwazamException;
+
+public class PeerListBackup {
+	private final String storagePath;
+	private final String fileName = "knownPeers.json";
+	
+	/**
+	 * Creates a new instance of PersistenceStorage with the given path.
+	 * @param storagePath the path were the json file should be stored
+	 */
+	public PeerListBackup(String storagePath) {
+		if(!(storagePath.lastIndexOf("/") == storagePath.length()-1))
+			storagePath += "/";
+		this.storagePath = storagePath;
+	}
+	
+	/**
+	 * Serializes the given Peerlist and stores the peer information to the file system. The file
+	 * is placed on the defined path having the defined file name. 
+	 * @param peers the list containing the peers that should be serialized
+	 * @throws SwazamException in case the storage is unsuccessful.
+	 */
+	public void storePeers(ArrayPeerList<InetSocketAddress> peers) throws SwazamException {
+		Gson gson = new Gson();
+		Type token = new TypeToken<ArrayPeerList<InetSocketAddress>>(){}.getType();
+		String json = gson.toJson(peers, token);
+		
+		try {
+			FileWriter writer = new FileWriter(storagePath + fileName);
+			writer.write(json);
+			writer.close();
+		} catch(IOException e) {
+			throw new SwazamException("Could not store peer information to file system. Cause was: " + e.getMessage());
+		}
+	}
+	
+	/**
+	 * Deserializes the stored peer information from the file system and creates a list containing all known peers.
+	 * @return a list containing all known peers.
+	 * @throws SwazamException in case loading the peer information from the file system was unsuccessful. 
+	 */
+	public ArrayPeerList<InetSocketAddress> loadPeers() throws SwazamException {
+		Gson gson = new Gson();
+		Type token = new TypeToken<ArrayPeerList<InetSocketAddress>>(){}.getType();
+		
+		try {
+			
+			BufferedReader br = new BufferedReader(new FileReader(storagePath + fileName));
+			ArrayPeerList<InetSocketAddress> list = gson.fromJson(br, token);
+			
+			return list;
+			
+		} catch (FileNotFoundException e) {
+			throw new SwazamException("Could not load peer information from file system. Cause was: " + e.getMessage());
+		}
+	}
+
+}
