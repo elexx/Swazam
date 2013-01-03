ba3fc4d92cb90aed46d6731ad4f4d90897e398a7
marcusmeisel <marcusmeisel@gmail.com>
2013-01-03 23:05:04 +0100

    nottin

--- Client/src/main/java/swa/swazam/client/App.java
+++ Client/src/main/java/swa/swazam/client/App.java
@@ -5,6 +5,6 @@ package swa.swazam.client;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World Client");
+		System.out.println("Hello World Client!");
 	}
 }
