3a8d2cd5e39bca994e5a5aef84eb64a4994090ef
Michael Borkowski <michael.borkowski@tuwien.ac.at>
2013-01-17 15:46:48 +0100

    fixed concurrent communication problem

--- Util/src/main/java/swa/swazam/util/communication/api/intern/net/PipelineFactoryFactory.java
+++ Util/src/main/java/swa/swazam/util/communication/api/intern/net/PipelineFactoryFactory.java
@@ -1,6 +1,5 @@
 package swa.swazam.util.communication.api.intern.net;
 
-import org.jboss.netty.channel.ChannelHandler;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.channel.ChannelUpstreamHandler;
@@ -9,15 +8,12 @@ import org.jboss.netty.channel.Channels;
 public class PipelineFactoryFactory {
 	public static ChannelPipelineFactory createFactoryWithHandler(final ChannelUpstreamHandler handler) {
 		return new ChannelPipelineFactory() {
-			private final ChannelHandler encoder = DataEncodingFactory.createEncoder();
-			private final ChannelHandler decoder = DataEncodingFactory.createDecoder();
-
 			@Override
 			public ChannelPipeline getPipeline() throws Exception {
 				ChannelPipeline pipeline = Channels.pipeline();
 
-				pipeline.addLast("encoder", encoder);
-				pipeline.addLast("decoder", decoder);
+				pipeline.addLast("encoder", DataEncodingFactory.createEncoder());
+				pipeline.addLast("decoder", DataEncodingFactory.createDecoder());
 				pipeline.addLast("handler", handler);
 
 				return pipeline;
